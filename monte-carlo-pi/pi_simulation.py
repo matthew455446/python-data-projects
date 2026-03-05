883644e6e9414044efb6acdd91acf98c3c1f7586
matthew455446 <appleysmiggle@gmail.com>
2026-03-05 15:27:53 -0600

    Upgrade Monte Carlo π simulation: add NumPy and Matplotlib plotting

--- monte-carlo-pi/pi_simulation.py
+++ monte-carlo-pi/pi_simulation.py
@@ -1,14 +1,30 @@
-import random
+import numpy as np
+import matplotlib.pyplot as plt
 
-inside = 0
-total = 100000
+points = 1000000  # Number of points
 
-for i in range(total):
-    x = random.random()
-    y = random.random()
+x = np.random.rand(points)
+y = np.random.rand(points)
 
-    if x*x + y*y <= 1:
-        inside += 1
+distance_squared = x**2 + y**2
+inside = distance_squared <= 1
 
-pi = 4 * inside / total
-print("Estimated π:", pi)
+pi_estimate = 4 * np.sum(inside) / points
+print(f"Estimated π with {points} points: {pi_estimate:.6f}")
+
+sample_size = 5000
+sample_indices = np.random.choice(points, sample_size, replace=False)
+
+plt.figure(figsize=(6,6))
+plt.scatter(x[sample_indices][inside[sample_indices]],
+            y[sample_indices][inside[sample_indices]],
+            color='green', s=1, label='Inside Circle')
+plt.scatter(x[sample_indices][~inside[sample_indices]],
+            y[sample_indices][~inside[sample_indices]],
+            color='red', s=1, label='Outside Circle')
+plt.gca().set_aspect('equal', adjustable='box')
+plt.title("Monte Carlo π Simulation")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.legend()
+plt.show()
